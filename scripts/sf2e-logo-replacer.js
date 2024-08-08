7ea1bb5884af9b1fdc6f1e980584515cdf4ff4ed
LiftVortex <85648890+LiftVortex@users.noreply.github.com>
2024-08-08 17:59:43 +0100

    Updated js and css
    Change character sheet search buttons to now direct to the starfinder equivelents.
    Added in styles for the remaining window type (familiar, loot, cards, party etc)
    Changed chat message colours for better readability.

--- scripts/sf2e-logo-replacer.js
+++ scripts/sf2e-logo-replacer.js
@@ -7,4 +7,40 @@ Hooks.on('renderCharacterSheetPF2e', (app, html, data) => {
       'width': 'auto',
     });
   }
+
+  // Select the button with data-compendium="pf2e.ancestries"
+  let ancestryButton = document.querySelector('a[data-compendium="pf2e.ancestries"]');
+  // Change the data-compendium attribute to "pf2e.st"
+  if (ancestryButton) {
+    ancestryButton.setAttribute('data-compendium', 'starfinder-field-test-for-pf2e.ancestries');
+  }
+
+  // Select the button with data-compendium="pf2e.backgrounds"
+  let backgroundButton = document.querySelector('a[data-compendium="pf2e.backgrounds"]');
+  // Change the data-compendium attribute to "pf2e.bg"
+  if (backgroundButton) {
+    backgroundButton.setAttribute('data-compendium', 'starfinder-field-test-for-pf2e.backgrounds');
+  }
+
+  // Select the button with data-compendium="pf2e.backgrounds"
+  let classButton = document.querySelector('a[data-compendium="pf2e.classes"]');
+  // Change the data-compendium attribute to "pf2e.bg"
+  if (classButton) {
+    classButton.setAttribute('data-compendium', 'starfinder-field-test-for-pf2e.classes');
+  }
+
+  // Select the button with data-compendium="pf2e.backgrounds"
+  let heritageButton = document.querySelector('a[data-compendium="pf2e.heritages"]');
+  // Change the data-compendium attribute to "pf2e.bg"
+  if (heritageButton) {
+    heritageButton.setAttribute('data-compendium', 'starfinder-field-test-for-pf2e.heritages');
+  }
+
+  // Select the button with data-compendium="pf2e.backgrounds"
+  let deityButton = document.querySelector('a[data-compendium="pf2e.deities"]');
+  // Change the data-compendium attribute to "pf2e.bg"
+  if (deityButton) {
+    deityButton.setAttribute('data-compendium', 'starfinder-field-test-for-pf2e.deities');
+  }
+
 });
