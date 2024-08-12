1519eb8551f64870ae2314b7603c0080b99f3f78
LiftVortex <85648890+LiftVortex@users.noreply.github.com>
2024-08-12 15:17:56 +0100

    Remade dark.css to be inline with light.css
    Complete rewrite of dark.css to match the style and formatting of light.css for maintainability.

--- scripts/sf2e-Overrides.js
+++ scripts/sf2e-Overrides.js
@@ -1,20 +1,4 @@
 Hooks.on('renderCharacterSheetPF2e', (app, html, data) => {
-  /*
-  const logoImg = html.find('.logo'); // Find the image with class 'logo'
-  if (logoImg.length) {
-    logoImg.attr('src', 'modules/sf2e-playtest-deluxe-adventure-pack/assets/art/acb/new_starfinder_logo-prototype.webp'); // Set the custom image source
-    logoImg.css({
-      'height': 'auto', // Maintain aspect ratio
-      'width': 'auto',
-    });
-  }*/
-
-  document.querySelectorAll('.window-content').forEach(element => {
-    if (element.querySelector('.char-header')) {
-      element.classList.add('char-header-container');
-    }
-  });
-  
 
   // Select the button with data-compendium="pf2e.ancestries"
   let ancestryButton = document.querySelector('a[data-compendium="pf2e.ancestries"]');
