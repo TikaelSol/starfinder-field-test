1e4d31ff31a4c49f7ecaa6a9d2b4def1716bec59
Carlos Fernandez <cfern1990@gmail.com>
2025-06-22 18:25:55 -0400

    Added watch command

--- build.mjs
+++ build.mjs
@@ -14,14 +14,31 @@ if (existsSync(outDir)) {
     await fs.mkdir(outDir);
 }
 
+console.log("Clean Finished");
+
 // Build packs
 const packFolders = await fs.readdir("packs");
 for (const pack of packFolders) {
     await compilePack(`packs/${pack}`, path.resolve(outDir, `packs/${pack}`));
 }
 
-// Copy files and folders to output
+console.log("Build Packs Finished");
+
 const files = ["art", "assets", "fonts", "lang", "scripts", "styles", "templates", "module.json", "ORCLicense.md"];
-for (const file of files) {
-    await fs.cp(file, path.resolve(outDir, file), { recursive: true });
+if (process.argv[2] === "--watch") {
+    const watcher = fs.watch(process.cwd(), { recursive: true });
+    console.log("Watching Files");
+    for await (const event of watcher) {
+        const file = event.filename.split(path.sep)[0];
+        if (files.includes(file)) {
+            await fs.cp(file, path.resolve(outDir, file), { recursive: true });
+            console.log("Files updated");
+        }
+    }
+} else {
+    // Copy files and folders to output
+    for (const file of files) {
+        await fs.cp(file, path.resolve(outDir, file), { recursive: true });
+    }
+    console.log("Build Complete");
 }
